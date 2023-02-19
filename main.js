38dd5f7170bc4b57721b7b09df45ac7841cc1ed6
Alexaloka <103824973+Alexaloka@users.noreply.github.com>
2023-02-19 14:50:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function take_snapshot()
     });
 }
 console.log('ml5 version:',ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/37VYaGEzs/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/khZjo2ziY/model.json',modelLoaded);
 function modelLoaded()
 {
     console.log("model loaded sucessfully");
